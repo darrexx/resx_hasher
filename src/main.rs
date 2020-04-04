b0795666d3f38830565675d954db6afe5b5bdc04
darrexx <timo.walter@udo.edu>
2020-04-04 12:14:04 +0200

    Refactor Code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use xml::reader::{EventReader, XmlEvent as ReaderEvent};
-use xml::writer::{EmitterConfig, XmlEvent as WriterEvent};
-use xml::name::Name;
+use xml::writer::{EmitterConfig, XmlEvent as WriterEvent, EventWriter};
+use xml::name::{Name, OwnedName};
+use xml::attribute::OwnedAttribute;
+use xml::namespace::Namespace;
 use hex::encode;
-use std::fs::{File};
-use walkdir::WalkDir;
+use std::fs::File;
+use walkdir::{WalkDir, DirEntry};
 use std::fs;
 use md5;
 use std::env;
 use std::io::BufReader;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -27,43 +30,23 @@ fn main() {
         if cfg!(debug_assertions) {
             println!("{:?}", entry.file_name());
         }
-        let file = File::open(entry.path()).unwrap();
-        let file = BufReader::new(file);
-        let reader = EventReader::new(file);
 
-        let mut new_file_path = entry.clone().into_path();
-        new_file_path.pop();
-        new_file_path.push("hashed");
-        fs::create_dir_all(&new_file_path).unwrap();
+        let reader = create_reader(entry.path());
 
-        new_file_path.push(entry.file_name());
-        if cfg!(debug_assertions) {
-            println!("{:?}", new_file_path);
-        }
-        let mut file = File::create(new_file_path).unwrap();
+        let mut file = create_hashed_file(entry);
         let mut writer = EmitterConfig::new().perform_indent(true).create_writer(&mut file);
-        let mut in_data =false;
+
+        let mut in_data = false;
         let mut in_comment = false;
-        let mut in_value =false;
+        let mut in_value = false;
         let mut localization_value: String = "".to_string();
 
         for e in reader {
             match e {
                 Ok(ReaderEvent::StartElement {name, attributes, namespace: _}) if name.local_name == "data" => {
                     in_data = true;
-                    let mut start_data = WriterEvent::start_element(name.borrow());
                     
-                    for attr in &attributes {
-                        let attr_name = Name{
-                            local_name: attr.name.local_name.as_str(),
-                            namespace: attr.name.namespace_ref(),
-                            prefix: attr.name.prefix_ref(),
-                        };
-
-                        start_data = start_data.attr(attr_name, attr.value.as_str());
-                    }
-    
-                    writer.write(start_data).unwrap();
+                    copy_and_write_start_element(name, attributes, &mut writer);
 
                     if cfg!(debug_assertions) {
                         println!("Start data");
@@ -73,18 +56,8 @@ fn main() {
                     if in_data{
                         in_value = true;
                     }
-                    let mut start_value = WriterEvent::start_element(name.borrow());
-                    for attr in &attributes {
-                        let attr_name = Name{
-                            local_name: attr.name.local_name.as_str(),
-                            namespace: attr.name.namespace_ref(),
-                            prefix: attr.name.prefix_ref(),
-                        };
 
-                        start_value = start_value.attr(attr_name, attr.value.as_str());
-                    }
-    
-                    writer.write(start_value).unwrap();
+                    copy_and_write_start_element(name, attributes, &mut writer);
     
                     if cfg!(debug_assertions) {
                         println!("Start value");
@@ -95,19 +68,7 @@ fn main() {
                         in_comment = true;
                     }
     
-                    let mut start_comment = WriterEvent::start_element(name.borrow());
-
-                    for attr in &attributes {
-                        let attr_name = Name{
-                            local_name: attr.name.local_name.as_str(),
-                            namespace: attr.name.namespace_ref(),
-                            prefix: attr.name.prefix_ref(),
-                        };
-
-                        start_comment = start_comment.attr(attr_name, attr.value.as_str());
-                    }
-    
-                    writer.write(start_comment).unwrap();
+                    copy_and_write_start_element(name, attributes, &mut writer);
                     
                     if cfg!(debug_assertions) {
                         println!("Start comment");
@@ -119,13 +80,7 @@ fn main() {
                         println!("{:?}", text);
                     }
                     if in_comment {
-                        let digest = md5::compute(&localization_value);
-                        let value = digest.0;
-                        let value_as_string = encode(&value);
-                        let mut md5 = "md5:".to_string();
-                        md5.push_str(value_as_string.as_str());
-    
-                        writer.write(WriterEvent::characters(&md5)).unwrap();
+                        create_and_write_md5_string(&localization_value, &mut writer);
                     }
                     else if in_value {
                         localization_value = text.clone();
@@ -143,13 +98,7 @@ fn main() {
                         let comment_start = WriterEvent::start_element("comment");
                         writer.write(comment_start).unwrap();
     
-                        let digest = md5::compute(&localization_value);
-                        let value = digest.0;
-                        let value_as_string = hex::encode(&value);
-                        let mut md5 = "md5:".to_string();
-                        md5.push_str(value_as_string.as_str());
-                        let comment_content = WriterEvent::characters(&md5);
-                        writer.write(comment_content).unwrap();
+                        create_and_write_md5_string(&localization_value, &mut writer);
     
                         let comment_end = WriterEvent::end_element();
                         writer.write(comment_end).unwrap();
@@ -192,83 +141,58 @@ fn main() {
         }
     }
 
-    
-    
+    fn create_reader(path: &Path) -> EventReader<BufReader<File>> {
+        let file = File::open(path).unwrap();
+        let file = BufReader::new(file);
+        EventReader::new(file)
+    }
 
-    // reader.trim_text(true);
-    // let mut writer = Writer::new(Cursor::new(Vec::new()));
-    // let mut localization_value: &[u8] = b"asdf";
-    // let mut in_data = false;
-    // let mut in_value = false;
-    // let mut in_comment = false;
-    // let mut buffer = Vec::new();
-    // loop{
-    //     match reader.read_event(&mut buffer){
-    //         Ok(Event::Start(ref e)) if e.name() == b"value" => {
-    //             in_value = true;
-    //             writer.write_event(
-    //                 Event::Start(
-    //                     BytesStart::borrowed(e.name().clone(), e.name().len())
-    //                 )
-    //             ).expect("Error adding event to writer");
-    //         },
-    //         Ok(Event::Start(ref e)) if e.name() == b"data" => {
-    //             in_data = true;
-    //             writer.write_event(
-    //                 Event::Start(
-    //                     BytesStart::borrowed(e.name().clone(), e.name().len())
-    //                 )
-    //             ).expect("Error adding event to writer");
-    //         },
-    //         Ok(Event::Start(ref e)) if e.name() == b"comment" => {
-    //             if in_data {
-    //                 in_comment = true
-    //             }
-    //             writer.write_event(
-    //                 Event::Start(
-    //                     BytesStart::borrowed(e.name().clone(), e.name().len())
-    //                 )
-    //             ).expect("Error adding event to writer");
-    //         },
-    //         Ok(Event::Text(_)) if in_comment => {
-    //             let hash = md5::compute(localization_value);
-    //             let bytes_text = BytesText::from_plain(&hash.0);
-    //             writer.write_event(Event::Text(bytes_text)).expect("Error adding event to writer");
+    fn create_hashed_file(entry: DirEntry) -> File {
+        let mut new_file_path = entry.clone().into_path();
+        new_file_path.pop();
+        new_file_path.push("hashed");
+        fs::create_dir_all(&new_file_path).unwrap();
 
-    //         },
-    //         Ok(Event::Text(ref e)) if in_value && in_data => {
-    //             in_value = false;
-    //             localization_value = e.clone().escaped();
-    //             writer.write_event(
-    //                 Event::Text(
-    //                     BytesText::from_escaped(e.escaped())
-    //                 )
-    //             ).expect("Error adding event to writer");
-    //         },
-    //         Ok(Event::End(ref e)) if e.name() == b"data" => {
-    //             if !in_comment {
-    //                 writer.write_event(
-    //                     Event::Start(
-    //                         BytesStart::owned(b"comment".to_vec(), "comment".len())
-    //                     )
-    //                 ).expect("Error adding event to writer");
+        new_file_path.push(entry.file_name());
+        if cfg!(debug_assertions) {
+            println!("{:?}", new_file_path);
+        }
+        File::create(new_file_path).unwrap()
+    }
 
-    //             }
-    //             in_comment = false;
-    //             in_data = false;
-    //             writer.write_event(
-    //                 Event::End(
-    //                     BytesEnd::borrowed(e.name().clone())
-    //                 )
-    //             ).expect("Error adding event to writer");
-    //         },
-    //         Ok(Event::Eof) => break,
-    //         Ok(e) => {
-    //             writer.write_event(e).expect("Error adding event to writer");
-    //         },
-    //         Err(_) => {
-    //             panic!("Error parsing xml file");
-    //         },
-    //     }
-    //}
+    fn copy_and_write_start_element(name: OwnedName, attributes: Vec<OwnedAttribute>, writer: &mut EventWriter<&mut File>){
+        let element_name = Name {
+            local_name: name.local_name.as_str(),
+            namespace: name.namespace_ref(),
+            prefix: name.prefix_ref(),
+        };
+        
+        let mut start_element = WriterEvent::start_element(element_name);
+                    
+        for attr in &attributes {
+            let attr_name = Name{
+                local_name: attr.name.local_name.as_str(),
+                namespace: attr.name.namespace_ref(),
+                prefix: attr.name.prefix_ref(),
+            };
+
+            start_element = start_element.attr(attr_name, attr.value.as_str());
+        }
+
+        writer.write(start_element).unwrap();
+    }
+
+    fn create_and_write_md5_string(localization_value: &String, writer: &mut EventWriter<&mut File>) {
+        let md5_string = create_md5_string(localization_value);
+        writer.write(WriterEvent::characters(&md5_string)).unwrap();
+    }
+
+    fn create_md5_string(localization_value: &String) -> String {
+        let digest = md5::compute(&localization_value);
+        let value = digest.0;
+        let value_as_string = encode(&value);
+        let mut md5 = "md5:".to_string();
+        md5.push_str(value_as_string.as_str());
+        md5
+    }
 }
